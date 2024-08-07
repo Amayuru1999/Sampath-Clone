f875ef060e28f6fb2f5abddf18be95ac4bac7c8a
Amayuru1999 <amarasingheau@gmail.com>
2024-08-07 22:05:10 +0530

    feat: Update TotalBalanceBox component to use DoughnutChart for displaying doughnut chart

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -8,8 +8,18 @@ ChartJS.register(ArcElement,Tooltip,Legend);
 
 
 const DoughnutChart = ({accounts}:DoughnutChartProps) => {
+    const data={
+        datasets: [
+            {
+                label: 'Banks',
+                data:[1250,2500,3750],
+                backgroundColor: ['#fce8d8','#f7a56f','#f47c22']
+            }
+        ],
+        labels:['Bank of Ceylon','Sampath Bank','Commercial Bank']
+    }
   return (
-    <Doughnut data={[]}/>
+    <Doughnut data={data}/>
   )
 }
 
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from '@/lib/utils'
 import React from 'react'
 import CountUp from 'react-countup'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts=[],totalBanks,totalCurrentBalance
@@ -9,7 +10,7 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
       <div className='total-balance-chart'>
-            {/* Doughnut Chart */}
+           <DoughnutChart accounts={accounts}/>
       </div>
       <div className='flex flex-col gap-6'>
         <h2 className='header-2'>
