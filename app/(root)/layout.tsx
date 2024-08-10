319eb447a6b1775e9e1e21b78cffa02a1e122bdc
Amayuru1999 <amarasingheau@gmail.com>
2024-08-10 23:02:57 +0530

    feat: Add @radix-ui/react-dialog dependency and implement Sheet component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,27 +2,29 @@ import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName: 'Amayuru',lastName: 'Amarasinghe'};
+  const loggedIn = { firstName: "Amayuru", lastName: "Amarasinghe" };
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
-        <div className="flex size-full flex-col">
-            <div className="root-layout">
-              <Image src="/icons/sampath.jpg" width={30} height={30} alt="menu icon"/>
-            <div>
-            <MobileNav user={loggedIn}/>
-            </div>
-            
-            </div> 
-            {children}
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/sampath.jpg"
+            width={30}
+            height={30}
+            alt="logo"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
         </div>
-        
+        {children}
+      </div>
     </main>
   );
 }
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 const MobileNav = ({user}:MobileNavProps) => {
   return (
     <div>
