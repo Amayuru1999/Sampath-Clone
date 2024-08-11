b54170808e7740db5eceaa9d4546262737a75b2d
Amayuru1999 <amarasingheau@gmail.com>
2024-08-11 19:19:59 +0530

    Update RightSidebar component to include bank list

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -31,6 +31,11 @@ const RightSidebar = ({user,transactions,banks}:RightSidebarProps) => {
         <h2 className='text-14 font-semibold text-gray-600'>Add Bank</h2>
         </Link>
       </div>
+      {banks?.length > 0 && (
+        <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
+
+        </div>
+      )}
     </section>
   </aside>
       
